3f7b4bb2e1294f35ec8080e9c8ba03bc6ce9a57f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 23:12:27 +0000

    Done.

--- square_arraytest.rb
+++ square_arraytest.rb
@@ -1,10 +1,17 @@
+numbers = [1,2,3]
+
 def square_array(array)
   counter = 0 
   new_array = []
   while counter < array.length do
+   # new_array = []
     item = array[counter]
     new_array << item**2
+    # puts new_array
+    # return new_array
     counter += 1
   end 
-  new_array
+  p new_array
 end 
+square_array(numbers)
+
